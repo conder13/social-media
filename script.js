70c0d03fd595bd1ce4cf8b27afbe19eb1cacd51d
conder <brenny.oshaughnessy@gmail.com>
2025-06-21 19:09:50 -0400

    local chat

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 const socket = io('http://localhost:3000');
+const messageForm = document.getElementById("messagecontainer");
+const messageInput = document.getElementById("m");
+const messageBoard = document.getElementById("messageboard");
+
+const usernameField = document.getElementById("usernamecontainer");
+const usernameInput = document.getElementById("username");
+var username = usernameInput.value;
+addMessage("You joined");
+socket.emit("new-user", username);
 
 socket.on('chat-message', data => {
    console.log(data);
+   addMessage(data["username"], data["message"]);
 })
+
+socket.on('user-joined', username => {
+   addMessage(`${username} joined the chat`);
+})
+
+socket.on('user-left', username => {
+   addMessage(`${username} left the chat`);
+})
+
+socket.on('changed-name', data => {
+   addMessage(data);
+})
+
+messageForm.addEventListener('submit', e => {
+   e.preventDefault();
+   const message = messageInput.value;
+   socket.emit('send-chat-message', message);
+   messageInput.value = "";
+   addMessage(username, message);
+})
+
+usernameField.addEventListener('submit', e => {
+   e.preventDefault();
+   username = usernameInput.value;
+   socket.emit("changed-username", username);
+   addMessage(`You became ${username}`);
+})
+
+function addMessage(name, text) {
+   var msg = document.createElement("li");
+   if (text != null) {
+      name = name + ": " + text;
+   }
+   msg.appendChild(document.createTextNode(name));
+   messageBoard.appendChild(msg);
+}
--- server.js
+++ server.js
@@ -4,8 +4,24 @@ const io = require("socket.io")(3000, {
       origin: "*",
    },
 });
+
+const users = {};
+
 //gives user a socket when loading page and sending message
 io.on('connection', socket => {
-   console.log("new user");
-   socket.emit('chat-message', 'Hello World');
+   //socket.emit('chat-message', 'Hello World');
+   socket.on('new-user', username => {
+      users[socket.id] = username;
+      console.log(username + " joined");
+      socket.broadcast.emit('user-joined', username);
+   })
+   socket.on('send-chat-message', message => {
+      console.log(message);
+      socket.broadcast.emit('chat-message', { message: message, username: users[socket.id] });
+   })
+   socket.on('changed-username', username => {
+      console.log(users[socket.id] + " changed their name to" + username);
+      socket.broadcast.emit('changed-name', (users[socket.id] + " changed their name to " + username));
+      users[socket.id] = username;
+   })
 })
